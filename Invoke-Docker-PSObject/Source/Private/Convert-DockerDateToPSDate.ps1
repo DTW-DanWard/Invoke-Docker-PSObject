76d25dfd6ea0040fe5048dc69f435b46aee52aa1
Dan Ward <dtward@gmail.com>
2018-10-06 21:21:31 -0400

    added Convert-DockerDateToPSDate tests

--- Invoke-Docker-PSObject/Source/Private/Convert-DockerDateToPSDate.ps1
+++ Invoke-Docker-PSObject/Source/Private/Convert-DockerDateToPSDate.ps1
@@ -11,8 +11,10 @@ function Convert-DockerDateToPSDate {
   #endregion
   process {
     # Example of Docker date: 2018-08-12 18:13:50 -0400 EDT
-    # first remove timezone suffix
-    $DockerDate =  $DockerDate.Substring(0, $DockerDate.IndexOf(' -'))
+    # first remove timezone suffix if exists
+    if (($DockerDate.IndexOf(' -')) -gt 0) {
+      $DockerDate =  $DockerDate.Substring(0, $DockerDate.IndexOf(' -'))
+    }
     # convert to proper PowerShell date and return
     [System.DateTime]::ParseExact($DockerDate, "yyyy-MM-dd HH:mm:ss", [CultureInfo]::InvariantCulture)
   }
--- Tests/Convert-DockerDateToPSDate.Tests.ps1
+++ Tests/Convert-DockerDateToPSDate.Tests.ps1
@@ -1,4 +1,6 @@
 
+Set-StrictMode -Version Latest
+
 # dot-source source file for this test file; exit if not found
 . $PSScriptRoot\Get-SourceScriptFilePath.ps1
 $SourceScript = Get-SourceScriptFilePath
@@ -6,7 +8,22 @@ if ($null -eq $SourceScript) { exit }
 . $SourceScript
 
 
+Describe "Converts Docker date string to System.DateTime object" {
+  It "converts valid Docker date strings with timezone correctly" {
+    Convert-DockerDateToPSDate '2018-07-19 20:39:27 -0400 EDT' | Should BeOfType 'datetime'
+  }
 
-# Valid date: 2018-07-19 20:39:27 -0400 EDT
-# Not valid:  2018-07-19 20:39:27
-# Not valid:  NOT_VALID
+  It "converts valid Docker date strings with no timezone correctly" {
+    Convert-DockerDateToPSDate '2018-07-19 20:39:27' | Should BeOfType 'datetime'
+  }
+}
+
+Describe "Throws exceptions for invalid date strings" {
+  It "throws errors for non-Docker date strings" {
+    { Convert-DockerDateToPSDate 'Saturday, October 6, 2018 9:19:27 PM' } | Should throw
+  }
+
+  It "throws errors for completely invalid date strings" {
+    { Convert-DockerDateToPSDate 'NOT_VALID_DATE_STRING' } | Should throw
+  }
+}
