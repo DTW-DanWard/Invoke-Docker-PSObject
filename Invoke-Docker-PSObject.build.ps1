0134bcd9da8b3b47bb924024c20a1e65311ee70f
Dan Ward <dtward@gmail.com>
2018-11-11 13:53:35 -0500

    improvement to PSScriptAnalyzer output and build Version updates driving soley off PSGallery published version and !major or !minor commit messages

--- Invoke-Docker-PSObject.build.ps1
+++ Invoke-Docker-PSObject.build.ps1
@@ -73,15 +73,16 @@ task Test Init, {
 }
 
 # Synopsis: Run unit tests in PowerShell Core Ubuntu Docker instance
-task Test_Ubuntu {
+task Test_Ubuntu Init, {
   $Line
   "`nTesting PowerShell in Ubuntu container"
 
+  # asdf needs error handling
   # this should only be run on local developer machine, not on build server, and should not be a
   # required part of the deployment to PowerShell Gallery
-  if ($env:BHBuildSystem -ne 'Unknown') { Write-Error 'Task Test_MultiOS should only be run on local dev machine' }
+  if ($env:BHBuildSystem -ne 'Unknown') { Write-Error 'Task Test_Ubuntu should only be run on local dev machine' }
 
-  # simple hard-coded version for now; use Ubuntu 16.04 on local machine
+  # simple hard-coded version for now; use Ubuntu 16.04 image on local machine
   $ContainerName = 'TestContainer'
   "`nStop and remove container with name: $ContainerName"
   docker stop $ContainerName
@@ -96,7 +97,7 @@ task Test_Ubuntu {
   docker cp $ProjectRoot ($ContainerName + ':/tmp')
   docker start $ContainerName
   "`nRun /build.ps1 Test"
-  # Invoke-Build fails if the /build.ps1 command is not run relative to the project - from the project root
+  # Invoke-Build fails if the /build.ps1 command is not run relative to the project - so run from project root
   docker exec $ContainerName pwsh -Command "& { cd /tmp/Invoke-Docker-PSObject; ./build.ps1 }"
   docker stop $ContainerName
 }
@@ -110,29 +111,57 @@ Task Analyze Init, {
   Get-ChildItem -Path $ProjectRoot -Recurse | Where-Object { @('.ps1', '.psm1') -contains $_.Extension -and $_.DirectoryName -ne $ProjectRoot } | ForEach-Object {
     $Results = Invoke-ScriptAnalyzer -Path $_.FullName
     if ($null -ne $Results) {
-      Write-Host "Bad results found for: $($_.Name)"
-      $Results
-      Write-Error "Fix ScriptAnalyzer results above"
+      Write-Build Red "PSScriptAnalyzer found issues in: $($_.Name)"
+      $Results | ForEach-Object {
+        Write-Build Red "$($_.Line) : $($_.Message)"
+      }
+      Write-Build Cyan "See full results with: Invoke-ScriptAnalyzer -Path $($_.FullName)"
+      Write-Error 'Fix above issues'
     }
   }
+  Write-Build Cyan "Analyze successful"
 }
-
-
 
 # Synopsis: Set public functions in PSD, increment version
 Task Build Test, Analyze, {
   $Line
+  "`nRunning Build"
+
+  #  # only run build if on build server on master
+  #  if (! ($env:BHBuildSystem -ne 'Unknown' -and $env:BHBranchName -eq 'master')) {
+  #    Write-Build Red 'Build only runs on build server on master branch'
+  #    return
+  #  }
 
   # Load the module, read the exported functions, update the psd1 FunctionsToExport
   Set-ModuleFunctions @Verbose
 
   # Bump the module version if we didn't manually bump it
   try {
-    $NextGalleryVersion = Get-NextPSGalleryVersion -Name $env:BHProjectName -ErrorAction Stop
-    $SourceVersion = Get-MetaData -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -ErrorAction Stop
-    if ($NextGalleryVersion -ge $SourceVersion) {
-      Update-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -Value $NextGalleryVersion -ErrorAction stop
+    # adapted from Get-NextNugetPackageVersion and PSGraph; should be refactored
+    # this only uses the publish version of the module when increment; whatever text in psd1 is ignored and overwritten
+    $PackageSourceUrl = 'https://www.powershellgallery.com/api/v2/'
+    $CurrentPackage = Find-NugetPackage -Name $env:BHProjectName -PackageSourceUrl $PackageSourceUrl -IsLatest -ErrorAction Stop
+    $CurrentVersion = '0.0.0'
+    if ($null -ne $CurrentPackage) {
+      $CurrentVersion = $CurrentPackage.Version
     }
+    "Current published version: $CurrentVersion"
+    # convert to version type so easier to work with
+    $CurrentVersion = [version]$CurrentVersion
+
+    if ($env:BHCommitMessage -match '!major') {
+      $NewVersion = New-Object System.Version (($CurrentVersion.Major + 1), 0, 0)
+    } elseif ($env:BHCommitMessage -match '!minor') {
+      $NewVersion = New-Object System.Version ($CurrentVersion.Major, ($CurrentVersion.Minor + 1), 0)
+    } else {
+      $NewVersion = New-Object System.Version ($CurrentVersion.Major, $CurrentVersion.Minor, ($CurrentVersion.Build + 1))
+    }
+
+    "New version: $NewVersion"
+    "Updating module metadata ModuleVersion and FunctionsToExport"
+    Update-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -Value ($NewVersion.ToString()) -ErrorAction Stop
+
   } catch {
     "Failed to update version for '$env:BHProjectName': $_.`nContinuing with existing version"
   }
@@ -142,6 +171,12 @@ Task Build Test, Analyze, {
 Task Deploy Build, {
   $Line
 
+  # only run build if on build server on master
+  if (! ($env:BHBuildSystem -ne 'Unknown' -and $env:BHBranchName -eq 'master')) {
+    Write-Build Red 'Build only runs on build server on master branch'
+    return
+  }
+  
   Write-Build Red 'Not implemented yet!'
 
   # $Params = @{
